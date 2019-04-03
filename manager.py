f6afb0ce875bf4a134d490a39d91af661f0e7394
OSinoooO <345285987@qq.com>
2019-04-04 02:08:20 +0800

    初始化项目配置

--- manager.py
+++ manager.py
@@ -1,8 +1,34 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+
+
+class Config(object):
+    """项目配置"""
+    DEBUG = True
+
+    # mysql配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:199577zhou@localhost:3306/news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
+
+    # redis配置
+    REDIS_HOST = 'localhost'
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# 初始化mysql对象
+db = SQLAlchemy(app)
+
+# 初始化redis对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启CSRF防护
+CSRFProtect(app)
 
 
 @app.route('/')
@@ -11,4 +37,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
