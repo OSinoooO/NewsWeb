9900ef99a1691be03121496c6eedbbf5157c0c34
OSinoooO <345285987@qq.com>
2019-04-04 08:38:25 +0800

    集成script和数据库迁移

--- manager.py
+++ manager.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -48,6 +50,13 @@ CSRFProtect(app)
 # 初始化session对象
 Session(app)
 
+manager = Manager(app)
+
+# 将app与db关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
+
 
 @app.route('/')
 def index():
@@ -56,4 +65,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
